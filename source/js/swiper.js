a4c2b5a71bd432bca982071325d474909e46023e
unknown <farid.gai0in@gmail.com>
2024-06-29 02:16:47 +0600

    fixes

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -78,7 +78,48 @@ function initSwiperAbout() {
   })
 }
 
+function initSwiperBig() {
+  const swiper = new Swiper('.swiper-big', {
+    speed: 450,
+    effect: 'coverflow',
+    rewind: true,
+    pagination: {
+      el: '.about-pagination',
+      type: 'progressbar',
+    },
+    updateOnWindowResize: true,
+    // loop: true,
+    spaceBetween: 20,
+    breakpoints: {
+      200: {
+        slideToClickedSlide: true,
+        coverflowEffect: {
+          rotate: 60,
+          stretch: 14,
+          depth: 500,
+          scale: 0.7,
+          modifier: 1,
+          slideShadows: false,
+        },
+        allowTouchMove: true,
+      },
+      1200: {
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 0,
+          depth: 0,
+          modifier: 0,
+          slideShadows: false,
+        },
+        allowTouchMove: false,
+        spaceBetween: 0,
+      },
+    },
+  })
+}
+
 $(document).ready(function () {
   initSwiperFinances()
   initSwiperAbout()
+  initSwiperBig()
 })
